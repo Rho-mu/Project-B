3c34509cb975cc6d8e97cdb6d17d69d29445327c
Riley McWilliams <31832109+Rho-mu@users.noreply.github.com>
2020-07-20 20:22:33 -0700

    Added turret building and camera controlls
    End of Brackeys tutorial 7.

---
+++ Project B/Assets/Scripts/BuildManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    public static BuildManager instance;
+
+    private GameObject turretToBuild;
+    public GameObject standardTurretPrefab;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.Log("BuildManager already exists!");
+        }
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        turretToBuild = standardTurretPrefab; // Defaults turretToBuild to the standard turret.
+    }
+
+    public GameObject GetTurretToBuild()
+    {
+        return turretToBuild;
+    }
+}
---
+++ Project B/Assets/Scripts/Node.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Node : MonoBehaviour
+{
+    public Color hoverColor;
+
+    private GameObject turret;
+    public Vector3 positionOffest;
+
+
+    private Renderer rend;
+    private Color startColor;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>(); // Caches the Renderer on game start rather than every time it's hovered.
+        startColor = rend.material.color;
+    }
+
+    void OnMouseDown()
+    {
+        if( turret != null )
+        {
+            Debug.Log("Turret already on this spot");
+            return;
+        }
+
+        GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+        turret = (GameObject)Instantiate(turretToBuild, transform.position + positionOffest, transform.rotation);
+    }
+
+    void OnMouseEnter()
+    {
+        rend.material.color = hoverColor;
+    }
+
+     void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+}
